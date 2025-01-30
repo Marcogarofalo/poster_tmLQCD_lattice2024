13c8421ab71db5d59e3f2d24cc3f921fe0db192d
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2025-01-30 16:32:32 +0100

    WIP of first steps, still a long way to go...

--- data/gf_observables/plot_gf_observables.R
+++ data/gf_observables/plot_gf_observables.R
@@ -1,12 +1,13 @@
 require(ggplot2)
 require(hadron)
 require(dplyr)
+require(patchwork)
 
-ensembles <- c("cAp211.085.48", "cB211.072.64", "cB211.072.96", "cC211.06.80", 
+ensembles <- c("cAp211.085.48", "cAp211.085.56", "cB211.072.64", "cB211.072.96", "cC211.06.80", 
                "cD211.054.96_BC", "cD211.054.96_DE", "cE211ab.044.112", "cE211cd.044.112")
-as <- c(0.087, 0.080, 0.080, 0.068, 
+as <- c(0.087, 0.087, 0.080, 0.080, 0.068, 
         0.057, 0.057,  0.049, 0.049)
-Ls <- c(48, 64, 96, 80, 
+Ls <- c(48, 56, 64, 96, 80, 
         96, 96, 112, 112)
 
 dat  <- NULL
@@ -34,14 +35,14 @@ for( i in 1:length(ensembles) ){
 
   md_offset <- 0
   if( ens == "cD211.054.96_DE" ){
-    md_offset <- 1250
+    md_offset <- max(filter(dat, ens == "cD211.054.96")$md_idx)
   } else if ( ens == "cE211cd.044.112" ){
-    md_offset <- 2200
+    md_offset <- max(filter(dat, ens == "cE211.044.112")$md_idx)
   }
 
   stream_id <- ""
   if( ens == "cD211.054.96_DE" || ens == "cE211cd.044.112" ){
-    stream_id <- "b"
+    stream_id <- ""
   }
 
   dat <- rbind(dat,
@@ -78,52 +79,115 @@ dat_stat <- dplyr::group_by(dat, ens) %>%
                              Qsym_w0_min = min(Qsym_w0)) %>%
             dplyr::ungroup()
 
-label_dat <- data.frame(ens = c(ensembles[1:4], "cD211.054.96", "cE211.044.112"),
-                        label = sprintf("$a \\sim %.3f$ fm, $L/a = %d$",
-                                        c(0.087, 0.080, 0.080, 0.068, 0.057, 0.049),
-                                        c(48, 64, 96, 80, 96, 112)),
-                        x = rep(2600, times=6),
-                        y_Wsym = dat_stat$Wsym_w0_min + 0.10*abs(dat_stat$Wsym_w0_max-dat_stat$Wsym_w0_min),
-                        y_Qsym = dat_stat$Qsym_w0_min + 0.10*abs(dat_stat$Qsym_w0_max-dat_stat$Qsym_w0_min))
+lim_dat <- data.frame(ens = rep(dat_stat$ens, each=2), 
+                      y_Wsym = as.vector(rbind(
+                                         dat_stat$Wsym_w0_min-0.25*abs(dat_stat$Wsym_w0_max-dat_stat$Wsym_w0_min), 
+                                         dat_stat$Wsym_w0_max+0.25*abs(dat_stat$Wsym_w0_max-dat_stat$Wsym_w0_min))),
+                      y_Qsym = as.vector(rbind(
+                                         dat_stat$Qsym_w0_min-0.25*abs(dat_stat$Qsym_w0_max-dat_stat$Qsym_w0_min), 
+                                         dat_stat$Qsym_w0_max+0.25*abs(dat_stat$Qsym_w0_max-dat_stat$Qsym_w0_min))))
+
+label_dat <- data.frame(ens = c(ensembles[1:5], "cD211.054.96", "cE211.044.112"),
+                        label = sprintf("$%s, a \\sim %.3f$ fm, $L/a = %d$",
+                                        c(ensembles[1:5], "cD211.054.96", "cE211.044.112"),
+                                        c(0.087, 0.087, 0.080, 0.080, 0.068, 0.057, 0.049),
+                                        c(48, 56, 64, 96, 80, 96, 112)),
+                        x = rep(740, times=7),
+                        y_Wsym = dat_stat$Wsym_w0_max - 0.10*abs(dat_stat$Wsym_w0_max-dat_stat$Wsym_w0_min),
+                        y_Qsym = dat_stat$Qsym_w0_max - 0.10*abs(dat_stat$Qsym_w0_max-dat_stat$Qsym_w0_min))
 
 tikzfiles <- hadron::tikz.init(basename = "gf_observables_md_histories", width = 3, height = 4)
 
-p <- ggplot2::ggplot(data = dat, 
+p1 <- ggplot2::ggplot(data = dat, 
                      aes(x = md_idx, y = Wsym_w0, colour = interaction(ens,stream,sep=""))) +
      ggplot2::geom_line() +
-     ggplot2::lims(x = c(0,3200)) +
+     ggplot2::lims(x = c(0,2200)) +
+     ggplot2::geom_point(aes(y = Wsym_w0),
+                         x = 1000, colour = NA, fill = NA,
+                         inherit.aes = FALSE) +
      ggplot2::facet_wrap("ens ~ .", ncol = 1, scales = 'free_y') +
      ggplot2::theme_bw() +
      ggplot2::geom_label(data = label_dat,
                         aes(x = x, y = y_Wsym, label = label),
-                        size = unit(2, 'pt'),
+                        size = unit(1.8, 'pt'),
                         inherit.aes = FALSE) + 
      ggplot2::labs(x = "$t_{\\mathrm{MD}}$",
-                   y = "$\\langle t \\frac{d}{dt} [ t^2 E_{\\mathrm{sym}}(t) ]\\rangle|_{t = (w_0/a)^2}$",
+                   y = "$t \\frac{d}{dt} [ t^2 E_{\\mathrm{sym}}(t) ]|_{t = w_0^2}$",
                    colour = "ensemble name") +
      ggplot2::theme(strip.background = element_blank(),
                     strip.text.x = element_blank(),
+                    plot.margin = margin(t = 0, r = 2, b = 0, l = 0),
                     legend.position = "none")
-plot(p)
 
-p <- ggplot2::ggplot(dat = dat, 
+p2 <- ggplot2::ggplot(data = dat, 
+                      aes(y = Wsym_w0, colour = interaction(ens,stream,sep=""))) +
+     ggplot2::geom_histogram(aes(after_stat(ndensity),
+                                 fill = interaction(ens,stream,sep=""))) +
+     ggplot2::facet_wrap("ens ~ .", ncol = 1, scales = 'free_y') +
+     ggplot2::theme_bw() +
+     ggplot2::labs(y = "",
+                   x = "",
+                   colour = "ensemble name") +
+     ggplot2::guides(y = "none", x = "none") +
+     ggplot2::theme(strip.background = element_blank(),
+                    strip.text.x = element_blank(),
+                    axis.text.x = element_blank(),
+                    axis.title = element_blank(),
+                    legend.position = "none",
+                    axis.ticks.length = unit(0, "pt"),
+                    plot.margin = margin(t = 0, r = 0, b = 0, l = 0))
+
+layout <- "AAAAAB"
+
+combined <- (p1 + p2)
+combined <- combined +
+            patchwork::plot_layout(guides = "collect", design = layout)#widths = c(1,1), heights = c(2,2))    
+plot(combined)    
+
+p1 <- ggplot2::ggplot(dat = dat, 
                      aes(x = md_idx, y = Qsym_w0, colour = interaction(ens,stream,sep=""))) +
      ggplot2::geom_line() +
+     ggplot2::geom_point(aes(y = Qsym_w0),
+                         x = 1000, colour = NA, fill = NA,
+                         inherit.aes = FALSE) +
      ggplot2::facet_wrap("ens ~ .", ncol = 1, scales = 'free_y') +
-     ggplot2::lims(x = c(0,3200)) +
+     ggplot2::lims(x = c(0,2200)) +
      ggplot2::theme_bw() +
      ggplot2::labs(x = "$t_{\\mathrm{MD}}$",
                    #y = "$\\langle \\varepsilon_{\\mu \\nu \\rho \\gamma}^{\\mathrm{(eucl)}} F_{\\mu \\nu} F_{\\rho \\gamma}(t) \\rangle|_{t=(w_0/a)^2}$",
-                   y = "$Q(t)|_{t=(w_0/a)^2}$",
+                   y = "$Q(t)|_{t=w_0^2}$",
                    colour = "ensemble name") +
      ggplot2::geom_label(data = label_dat,
                         aes(x = x, y = y_Qsym, label = label),
-                        size = unit(2, 'pt'),
+                        size = unit(1.8, 'pt'),
                         inherit.aes = FALSE) + 
      ggplot2::theme(strip.background = element_blank(),
                     strip.text.x = element_blank(),
+                    plot.margin = margin(t = 0, r = 2, b = 0, l = 0),
                     legend.position = "none")
-plot(p)
+
+p2 <- ggplot2::ggplot(data = dat, 
+                      aes(y = Qsym_w0, colour = interaction(ens,stream,sep=""))) +
+     ggplot2::geom_histogram(aes(after_stat(ndensity),
+                                 fill = interaction(ens,stream,sep=""))) +
+     ggplot2::facet_wrap("ens ~ .", ncol = 1, scales = 'free_y') +
+     ggplot2::theme_bw() +
+     ggplot2::labs(y = "",
+                   x = "",
+                   colour = "ensemble name") +
+     ggplot2::guides(y = "none", x = "none") +
+     ggplot2::theme(strip.background = element_blank(),
+                    strip.text.x = element_blank(),
+                    axis.text.x = element_blank(),
+                    axis.title = element_blank(),
+                    legend.position = "none",
+                    axis.ticks.length = unit(0, "pt"),
+                    plot.margin = margin(t = 0, r = 0, b = 0, l = 0))
+
+combined <- (p1 + p2)
+combined <- combined +
+            patchwork::plot_layout(guides = "collect", design = layout)#widths = c(1,1), heights = c(2,2))    
+plot(combined)    
 
 hadron::tikz.finalize(tikzfiles)
 
